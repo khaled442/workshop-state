e48422eee303cdb7997ade68d23a53db9d2adf1b
khaled442 <boudayakhaled@gmail.com>
2021-07-11 01:40:07 +0100

    workshopstate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+export class App extends Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    this.state = {
+      count: 0,
+      toogler: true,
+      img: "me.jpg",
+      fullname: "Boudaya Khaled",
+      bio: "learning Web devlp",
+      profession: "futur Js Dev",
+    };
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ count: this.state.count + 1 });
+    }, 2000);
+  }
+  toogle = () => {
+    this.setState({
+      toogler: !this.state.toogler,
+      count: 0,
+    });
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginTop: "40px",
+          border: "3px solid gray",
+          width: "500px",
+          marginLeft: "450px",
+          height: "600px",
+        }}
+      >
+        {this.state.toogler ? (
+          <div style={{ marginTop: "50px" }}>
+            <img
+              style={{ width: "200px", height: "200px", marginTop: "-20px" }}
+              src={this.state.img}
+              alt=""
+            />
+            <h1>FullName: {this.state.fullname}</h1>
+            <h1>Bio :{this.state.bio}</h1>
+            <h1>Profession: {this.state.profession}</h1>
+          </div>
+        ) : (
+          <h1 style={{ color: "red" }}>Welcome to my profile</h1>
+        )}
+        <Button
+          style={{ marginLeft: "10px", marginTop: "20px" }}
+          variant="dark"
+          onClick={this.toogle}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          Show Profile
+        </Button>
 
+        <h1 style={{ marginTop:'40px', color:'red'}}>
+          Time :<span>{this.state.count}</span>
+        </h1>
+      </div>
+    );
+  }
+}
 export default App;
